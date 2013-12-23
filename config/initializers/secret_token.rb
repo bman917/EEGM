5d1ae424b773e2553db45e01e55a93b36cf36bad
Mr Jacky Chan <bman917@gmail.com>
2013-12-24 02:30:16 +0800

    add secret token to avoid rail server error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eegm::Application.config.secret_key_base = 'e8c2df0cc5284bdeb4cae44ee0eef09eb08a6ba99700e864424b065ce8fec74e10c3ee24f057cec160cf46a1bea183a19e281061446b1ee130417825a2e46e2a'
