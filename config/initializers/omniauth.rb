2efd3f14b4d5fa83139dd250fb522ffab6c1f578
Mr Jacky Chan <bman917@gmail.com>
2014-01-05 13:58:08 +0800

    get google key and secret from env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "1042987663486.apps.googleusercontent.com", "2LN_Yl1hKTHQpcXUSeJaMZfW",
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"],
 	  {
 	  	:scope => "userinfo.email, https://www.google.com/m8/feeds/"
 	  }
